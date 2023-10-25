8cdec6a5052a84c66d65a3d05e39a19dc321210c
Frank Bruno <frannk.bruno@gmail.com>
2023-10-24 22:50:30 -0300

    adicionado movimento ao pacman

--- C/curso3/fogefoge.c
+++ C/curso3/fogefoge.c
@@ -51,8 +51,57 @@ void    lemapa(){
     fclose(f);
 }
 
+void    imprimemapa(){
+    for(int i = 0; i <5; i++){
+        printf("%s\n", mapa[i]);
+    }
+}
+
+void    move(char direcao){
+    int x;
+    int y;
+
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            if(mapa[i][j] == '@'){
+                x = i;
+                y = j;
+                break;
+            }
+        }
+    }
+
+    switch(direcao){
+        case 'a':
+            mapa[x][y-1] = '@';
+            break;
+        case 'w':
+            mapa[x-1][y] = '@';
+            break;
+        case 's':
+            mapa[x+1][y] = '@';
+            break;
+        case 'd':
+            mapa[x][y+1] = '@';
+            break;
+    }
+    mapa[x][y] = '.';
+}
+
+int acabou(){
+    return (0);
+}
+
 int main()
 {
     lemapa();
+do{
+    imprimemapa();
+
+    char comando;
+    scanf(" %c", &comando);
+    move(comando);
+}  while(!acabou());
+
     liberamapa();
 }
