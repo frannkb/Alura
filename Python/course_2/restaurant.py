05f51d5fe505f19d1cd739790a28f7a859aa3a8e
Frank Bruno <frannk.bruno@gmail.com>
2024-04-25 23:45:48 -0300

    Working with property and classmethor

--- Python/course_2/restaurant.py
+++ Python/course_2/restaurant.py
@@ -1,20 +1,29 @@
 class Restaurant:
 	restaurants = []
 
-	def __init__(self, name, category):
-		self.name = name
-		self.category = category
-		self.active = False
+	def __init__(self, name, category): #initialize
+		self.name = name.title()
+		self.category = category.upper()
+		self._active = False
 		Restaurant.restaurants.append(self)
 
 	def __str__(self):
 		return f'{self.name} | {self.category}'
-	
-	def list_restaurants():
-		for restaurant in Restaurant.restaurants:
-			print(f'{restaurant.name} | {restaurant.category} | {restaurant.active}')
+	@classmethod
+	def list_restaurants(cls):
+		print(f'{'Restaurant Name'.ljust(25)} | {'Category Name'.ljust(25)} | {'Active?'}')
+		for restaurant in cls.restaurants:
+			print(f'{restaurant.name.ljust(25)} | {restaurant.category.ljust(25)} | {restaurant.active}')
 
-restaurant_square = Restaurant('Square', 'Gourmet')
-restaurant_pizza = Restaurant('Pizza Express', 'Italian')
+	@property
+	def active(self):
+		return '⌧' if self._active else '☐'
+	
+	def change_state(self):
+		self._active = not self._active
+
+restaurant_square = Restaurant('square', 'Gourmet')
+restaurant_square.change_state()
+restaurant_pizza = Restaurant('pizza Express', 'Italian')
 
 Restaurant.list_restaurants()
