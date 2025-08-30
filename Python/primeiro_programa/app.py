1acec0b8806c7fdf263187668a798c5ff82650f2
Frank Bruno <frannk.bruno@gmail.com>
2025-08-30 09:37:46 -0300

    Módulos e funções

--- Python/primeiro_programa/app.py
+++ Python/primeiro_programa/app.py
@@ -1,12 +1,43 @@
-print("""
-Ｓａｂｏｒ Ｅｘｐｒｅｓｓ
-""")
+import os
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
+# Funções:
 
-opcao_escolhida = input('Escolha uma opção:')
-print(f'Voce escolheu a opção{opcao_escolhida}')
+def exibir_nome_do_programa():
+    print("""
+    Ｓａｂｏｒ Ｅｘｐｒｅｓｓ
+    """)
 
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o App\n')
+
+
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção:'))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app()
+
+
+
+# -- Definições do main para inicio de programa
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
